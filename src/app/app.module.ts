4c8cad36d738772ccfca5ac3729d3e903fc81419
Lrodriguez72 <LoreR1972@gmail.com>
2023-03-15 11:05:34 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OtroComponenteComponent } from './otro-componente/otro-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OtroComponenteComponent
   ],
   imports: [
     BrowserModule,
